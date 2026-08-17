59a856e30243aaf1a61877ad2a91d9d0535c66d1
VanessaMuniz <muniz.ravanessa@gmail.com>
2026-08-17 11:11:26 -0300

    Criando a classe Paciente,seus atributos e seu construtor

--- src/main/java/com/vanessa/nutriapp/Main.java
+++ src/main/java/com/vanessa/nutriapp/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
         Saudacao saudacao = new Saudacao();
         saudacao.darBoasVindas();
+
+        Paciente paciente1 = new Paciente("Ana", 33, 78.00,1.75);
+        Paciente paciente2 = new Paciente("Carlos", 38, 85.00,1.85);
+
+        paciente1.exibirPaciente();
+        paciente2.exibirPaciente();
     }
 }
---
+++ src/main/java/com/vanessa/nutriapp/Paciente.java
@@ -0,0 +1,23 @@
+package com.vanessa.nutriapp;
+
+public class Paciente {
+
+    String nome;
+    int idade;
+    double peso;
+    double altura;
+
+public Paciente(String nome, int idade, double peso, double altura) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+}
+public void exibirPaciente(){
+    System.out.println("Nome: " + this.nome);
+    System.out.println("Idade: " + this.idade + "anos");
+    System.out.println("Peso: " + this.peso + "Kg");
+    System.out.println("Altura: " + this.altura + "m");
+}
+
+}
